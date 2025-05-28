8427926b7d40608f45926102801fa7d5350afe33
RISH-VERSION <harishff007113@gmail.com>
2025-05-28 14:59:01 +0530

    saved

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home(props) {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-100 via-white to-blue-200">
+            <h1 className="text-5xl font-extrabold text-blue-700 shadow-2xl p-8 bg-white rounded-2xl border border-blue-200">
+                    
+            </h1>
+        </div>
+    );
+}
+
+export default Home;
