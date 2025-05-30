ffe46d44ee70751d1e369ae6ec6e05b9b7d5561c
SARAVANAKUMAR-05 <saravanamohanraj005@gmail.com>
2025-05-31 01:08:21 +0530

    Complete BMI calculator

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
+import BMI from './pages/BMI.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <BMI />
   </StrictMode>
 )
