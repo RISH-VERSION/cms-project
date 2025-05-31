582f65a211b3a0ed10e58014fd2e106a51463b9b
SARAVANAKUMAR-05 <saravanamohanraj005@gmail.com>
2025-05-31 09:58:40 +0530

    commit changed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
-import BMI from './pages/BMI.jsx';
-import Dietplan from './pages/Dietplan.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <Dietplan/>
+    <App />
   </StrictMode>
 )
